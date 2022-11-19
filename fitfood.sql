4841ede8f60f785b803a07d8dab30f49462661c3
Khanh Duy <tranhakhanhduy2001@gmail.com>
2022-11-19 11:38:12 +0700

    database: build er diagram mysql

--- fitfood.sql
+++ fitfood.sql
@@ -16,7 +16,7 @@ CREATE TABLE User(
 
 
 -- Table Customer
-create TABLE Customer(
+CREATE TABLE Customer(
 	ID int NOT NULL AUTO_INCREMENT,
     Name varchar(100) NOT NULL,
     DayOfBirth date NOT NULL,
@@ -35,20 +35,19 @@ create TABLE Customer(
     CONSTRAINT FK_Customer_Username FOREIGN KEY(Username) REFERENCES User(Username)
 );
 
-create TABLE Employee(
+-- Table Employee
+CREATE TABLE Employee(
 	ID int NOT NULL AUTO_INCREMENT,
     Name varchar(100) NOT NULL,
     DayOfBirth date NOT NULL,
     PhoneNumber varchar(10) NOT NULL,
-    Role varchar(20) NOT NULL,
-    Salary int NOT NULL,
     Gender int NOT NULL,
     Province varchar(10) NOT NULL,
     District varchar(10) NOT NULL,
     Ward varchar(10),
     Address varchar(255) NOT NULL,
-    UserID int NOT NULL,
 	Username varchar(100) NOT NULL,
+	Role varchar(20) NOT NULL,
 	CONSTRAINT CK_Employee_PhoneNumber UNIQUE(PhoneNumber),
 	CONSTRAINT CK_Employee_Gender CHECK(Gender=0 OR Gender=1),
     CONSTRAINT CK_Employee_Username UNIQUE(Username),
@@ -56,7 +55,101 @@ create TABLE Employee(
     CONSTRAINT FK_Employee_User FOREIGN KEY(Username) REFERENCES User(Username)
 );
 
+-- Table Provider
+CREATE TABLE Provider(
+	ID int NOT NULL AUTO_INCREMENT,
+    Name varchar(100) NOT NULL,
+    PhoneNumber varchar(10) NOT NULL,
+    Address varchar(255) NOT NULL,
+    CONSTRAINT PK_Provider_ID PRIMARY KEY(ID)
+);
 
+-- Table TypeProduct
+CREATE TABLE TypeProduct(
+	ID int NOT NULL AUTO_INCREMENT,
+    Name varchar(100) NOT NULL,
+    CONSTRAINT PK_TypeProduct_ID PRIMARY KEY(ID)
+);
+-- Table Product
+CREATE TABLE Product(
+	ID int NOT NULL AUTO_INCREMENT,
+    Name varchar(100) NOT NULL,
+    Price int NOT NULL,
+    Quantity int NOT NULL,
+	Unit varchar(10) NOT NULL,
+    TypeProductID int NOT NULL,
+	CONSTRAINT PK_Product_Id PRIMARY KEY(ID),
+    CONSTRAINT FK_Product_TypeProduct FOREIGN KEY(TypeProductID) REFERENCES TypeProduct(ID)
+);
+-- Table Bill
+CREATE TABLE Bill(
+	ID int NOT NULL AUTO_INCREMENT,
+    Date date NOT NULL,
+    State varchar(20) Not NULL,
+    CustomerID int NOT NULL,
+    EmployeeID int,
+    CONSTRAINT PK_Bill_Id PRIMARY KEY(ID),
+    CONSTRAINT FK_Bill_Customer FOREIGN KEY(CustomerID) REFERENCES Customer(ID),
+    CONSTRAINT FK_Bill_Employee FOREIGN KEY(EmployeeID) REFERENCES Employee(ID)
+    
+);
+
+-- Table DetailBill
+CREATE TABLE DetailBill(
+	BillID int NOT NULL,
+    ProductID int NOT NULL,
+    Quantity int NOT NULL,
+    SalePrice int NOT NULL,
+    CONSTRAINT PK_DetailBill_PK PRIMARY KEY(BillID,ProductID),
+    CONSTRAINT FK_DetailBill_Bill FOREIGN KEY(BillID) REFERENCES Bill(ID),
+    CONSTRAINT FK_DetailBill_Product FOREIGN KEY(ProductID) REFERENCES Product(ID)
+);
+
+-- Table DeliveryNote
+CREATE TABLE DeliveryNote(
+	ID int NOT NULL AUTO_INCREMENT,
+    Date date NOT NULL,
+    ProviderID int NOT NULL,
+    EmployeeID int NOT NULL,
+    CONSTRAINT PK_DeliveryNote_Id PRIMARY KEY(ID),
+    CONSTRAINT FK_DeliveryNote_Provider FOREIGN KEY(ProviderID) REFERENCES Provider(ID),
+    CONSTRAINT FK_DeliveryNote_Employee FOREIGN KEY(EmployeeID) REFERENCES Employee(ID)
+);
+
+-- Table DetailDeliveryNote
+CREATE TABLE DetailDeliveryNote(
+	DeliveryNoteID int NOT NULL,
+    ProductID int NOT NULL,
+    Quantity int NOT NULL,
+    ImportPrice int NOT NULL,
+    CONSTRAINT PK_DetailDeliveryNote_PK PRIMARY KEY(DeliveryNoteID,ProductID),
+    CONSTRAINT FK_DetailDeliveryNote_DeliveryNote FOREIGN KEY(DeliveryNoteID) REFERENCES DeliveryNote(ID),
+    CONSTRAINT FK_DetailDeliveryNote__Product FOREIGN KEY(ProductID) REFERENCES Product(ID)
+);
+
+-- Table Cart
+CREATE TABLE Cart(
+	CustomerID int NOT NULL,
+    ProductID int NOT NULL,
+    Quantity int NOT NULL,
+    CONSTRAINT PK_Cart_Customer_Product PRIMARY KEY(CustomerID,ProductID),
+    CONSTRAINT FK_Cart_Cusotmer FOREIGN KEY(CustomerID) REFERENCES Customer(ID),
+    CONSTRAINT FK_Cart_Product FOREIGN KEY(ProductID) REFERENCES Product(ID)
+);
+
+-- Table Review
+CREATE TABLE Review(
+	CustomerID int NOT NULL,
+    ProductID int NOT NULL,
+    Score int NOT NULL,
+    Comment varchar(500),
+	CONSTRAINT PK_Review_Customer_Product PRIMARY KEY(CustomerID,ProductID),
+	CONSTRAINT FK_Review_Cusotmer FOREIGN KEY(CustomerID) REFERENCES Customer(ID),
+    CONSTRAINT FK_Review_Product FOREIGN KEY(ProductID) REFERENCES Product(ID),
+    CONSTRAINT CK_Review_Score CHECK(Score >=1 and Score <=5)
+);
+
+-- Table 
 INSERT INTO User (`username`,`password`,`isAdmin`)
 VALUES ('Trần Hạ Khánh Duy ẮN ẲN ẴN BÁNH MÌ MÈO MÃ MỒ MẠ MÀI MIỆT1','12312456',false);
 
