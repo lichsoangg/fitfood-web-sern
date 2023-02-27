f967b7bf59f96c388a2880b7a518e7c77716a812
Khanh Duy <tranhakhanhduy2001@gmail.com>
2023-02-27 21:26:07 +0700

    chore: change table name for run scripts on ubuntu server

--- fitfood.sql
+++ fitfood.sql
@@ -2,13 +2,17 @@
 
 USE fitfood;
 
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345678';
-flush privileges;
+-- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345678';
+-- flush privileges;
 
  SET SQL_SAFE_UPDATES = 0;
+
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS; 
+SET FOREIGN_KEY_CHECKS=0;   
  -- 1: Admin, 2: Customer
  -- 1: Non Active, 2: Active
  -- 1: Male, 2: Female
+
 -- Table User
 CREATE TABLE user(
 	Username varchar(100) NOT NULL,
@@ -95,10 +99,10 @@ CREATE TABLE rating(
 );
 
 -- Data User
-Insert Into User(Username,Password, Role, IsActive, Name, DayOfBirth, PhoneNumber, Gender, Province, District, Ward, Address, Avatar) Values(
+Insert Into user(Username,Password, Role, IsActive, Name, DayOfBirth, PhoneNumber, Gender, Province, District, Ward, Address, Avatar) Values(
 'duytran@gmail.com','$2b$10$3vZxIa867MB5iQw6BNQhtOel/fpsqc5WqkaGFLEmrcn3hGmo/B3IC',2,2,'Khánh Duy','2000-09-01','0333121131','1','79','773','27283','Ho Chi Minh','EmployeeAvatar_1.png'
 );
-Insert Into User(Username,Password, Role, IsActive, Name, DayOfBirth, PhoneNumber, Gender, Province, District, Ward, Address, Avatar) Values(
+Insert Into user(Username,Password, Role, IsActive, Name, DayOfBirth, PhoneNumber, Gender, Province, District, Ward, Address, Avatar) Values(
 'duytran1@gmail.com','$2b$10$3vZxIa867MB5iQw6BNQhtOel/fpsqc5WqkaGFLEmrcn3hGmo/B3IC',2,2,'Khánh Duy','2000-09-01','0333121132','1','79','773','27283','Ho Chi Minh','EmployeeAvatar_2.png'
 );
 
@@ -106,85 +110,84 @@ Insert Into User(Username,Password, Role, IsActive, Name, DayOfBirth, PhoneNumbe
 
 -- Data ProductType
 
-INSERT INTO ProductType (`Name`) Values ('Đồ uống');
-INSERT INTO ProductType (`Name`) Values ('Đồ ăn');
+INSERT INTO producttype (`Name`) Values ('Đồ uống');
+INSERT INTO producttype (`Name`) Values ('Đồ ăn');
 
 
 -- Data Product
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('NƯỚC MÁT THẢO MỘC',100000,15,'Chai',1,'ProductAvatar_1.png','1');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('FITFOOD JUICE SWEETIE',200000,100,'Chai',2,'ProductAvatar_2.png','1');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('FITFOOD JUICE GREENIE',100000,100,'Chai',2,'ProductAvatar_3.png','1');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('BOX Ức gà mềm mọng',379000,100,'Hộp',1,'ProductAvatar_4.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Cơm gạo lức ăn liền ',100000,80,'Gói',1,'ProductAvatar_5.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Nhân Burger Gà Teriyaki',100000,87,'Gói',2,'ProductAvatar_6.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Ức gà ăn liền ',199000,100,'Hộp',1,'ProductAvatar_7.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gạo lứt Rong Biển',100000,80,'Hộp',1,'ProductAvatar_8.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gạo lứt Chà Bông',100000,100,'Hộp',1,'ProductAvatar_9.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Bánh ngói hạnh nhân',129000,100,'Hộp',1,'ProductAvatar_10.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Biscotti vị socola',129000,97,'Hộp',1,'ProductAvatar_11.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Biscotti vị trà xanh ',100000,100,'Hộp',2,'ProductAvatar_12.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Biscotti vị truyền thống',100000,100,'Hộp',2,'ProductAvatar_13.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Tempeh Tương Nén',100000,100,'Gói',2,'ProductAvatar_14.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Bánh mì ngũ cốc',85000,100,'Hộp',2,'ProductAvatar_15.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói FIT 1',600000,100,'Gói',1,'ProductAvatar_16.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói FIT 2',600000,100,'Gói',1,'ProductAvatar_17.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói FIT 3',600000,100,'Gói',1,'ProductAvatar_18.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói MEAT-S',1200000,100,'Gói',1,'ProductAvatar_19.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói MEAT',950000,100,'Gói',1,'ProductAvatar_20.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói chay',600000,100,'Gói',1,'ProductAvatar_21.png','2');
-Insert Into Product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói Full',750000,100,'Gói',1,'ProductAvatar_21.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('NƯỚC MÁT THẢO MỘC',100000,15,'Chai',1,'ProductAvatar_1.png','1');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('FITFOOD JUICE SWEETIE',200000,100,'Chai',2,'ProductAvatar_2.png','1');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('FITFOOD JUICE GREENIE',100000,100,'Chai',2,'ProductAvatar_3.png','1');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('BOX Ức gà mềm mọng',379000,100,'Hộp',1,'ProductAvatar_4.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Cơm gạo lức ăn liền ',100000,80,'Gói',1,'ProductAvatar_5.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Nhân Burger Gà Teriyaki',100000,87,'Gói',2,'ProductAvatar_6.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Ức gà ăn liền ',199000,100,'Hộp',1,'ProductAvatar_7.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gạo lứt Rong Biển',100000,80,'Hộp',1,'ProductAvatar_8.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gạo lứt Chà Bông',100000,100,'Hộp',1,'ProductAvatar_9.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Bánh ngói hạnh nhân',129000,100,'Hộp',1,'ProductAvatar_10.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Biscotti vị socola',129000,97,'Hộp',1,'ProductAvatar_11.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Biscotti vị trà xanh ',100000,100,'Hộp',2,'ProductAvatar_12.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Biscotti vị truyền thống',100000,100,'Hộp',2,'ProductAvatar_13.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Tempeh Tương Nén',100000,100,'Gói',2,'ProductAvatar_14.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Bánh mì ngũ cốc',85000,100,'Hộp',2,'ProductAvatar_15.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói FIT 1',600000,100,'Gói',1,'ProductAvatar_16.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói FIT 2',600000,100,'Gói',1,'ProductAvatar_17.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói FIT 3',600000,100,'Gói',1,'ProductAvatar_18.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói MEAT-S',1200000,100,'Gói',1,'ProductAvatar_19.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói MEAT',950000,100,'Gói',1,'ProductAvatar_20.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói chay',600000,100,'Gói',1,'ProductAvatar_21.png','2');
+Insert Into product (Name, Price, Quantity, Unit, Highlight,Avatar, ProductTypeID) Values ('Gói Full',750000,100,'Gói',1,'ProductAvatar_21.png','2');
 
 
 
 
 -- Data Bill
-Insert Into Bill (Date, State, Username) Values ('2020-10-11',1,'duytran@gmail.com');
-Insert Into Bill (Date, State, Username) Values ('2020-10-11',2,'duytran@gmail.com');
-Insert Into Bill (Date, State, Username) Values ('2021-10-11',1,'duytran@gmail.com');
-Insert Into Bill (Date, State, Username) Values ('2021-10-11',1,'duytran1@gmail.com');
+Insert Into bill (Date, State, Username) Values ('2020-10-11',1,'duytran@gmail.com');
+Insert Into bill (Date, State, Username) Values ('2020-10-11',2,'duytran@gmail.com');
+Insert Into bill (Date, State, Username) Values ('2021-10-11',1,'duytran@gmail.com');
+Insert Into bill (Date, State, Username) Values ('2021-10-11',1,'duytran1@gmail.com');
 -- Data Detail Bill
 
-Insert Into DetailBill (BillID, ProductID,Quantity, SalePrice) Values ('1','1',11,'150000');
-Insert Into DetailBill (BillID, ProductID,Quantity, SalePrice) Values ('1','3',5,'100000');
-Insert Into DetailBill (BillID, ProductID,Quantity, SalePrice) Values ('2','2',7,'150000');
-Insert Into DetailBill (BillID, ProductID,Quantity, SalePrice) Values ('2','13',8,'130000');
-Insert Into DetailBill (BillID, ProductID,Quantity, SalePrice) Values ('3','12',2,'150000');
-Insert Into DetailBill (BillID, ProductID,Quantity, SalePrice) Values ('3','13',1,'210000');
-Insert Into DetailBill (BillID, ProductID,Quantity, SalePrice) Values ('4','5',1,'150000');
-Insert Into DetailBill (BillID, ProductID,Quantity, SalePrice) Values ('4','6',2,'200000');
+Insert Into detailbill (BillID, ProductID,Quantity, SalePrice) Values ('1','1',11,'150000');
+Insert Into detailbill (BillID, ProductID,Quantity, SalePrice) Values ('1','3',5342,'100000');
+Insert Into detailbill (BillID, ProductID,Quantity, SalePrice) Values ('2','2',7123,'150000');
+Insert Into detailbill (BillID, ProductID,Quantity, SalePrice) Values ('2','13',832,'130000');
+Insert Into detailbill (BillID, ProductID,Quantity, SalePrice) Values ('3','12',239892,'150000');
+Insert Into detailbill (BillID, ProductID,Quantity, SalePrice) Values ('3','13',13123,'210000');
+Insert Into detailbill (BillID, ProductID,Quantity, SalePrice) Values ('4','5',1123222,'150000');
+Insert Into detailbill (BillID, ProductID,Quantity, SalePrice) Values ('4','6',2123,'200000');
 
 
 -- Data Cart
-Insert Into Cart(Username, ProductID, Quantity) Values('duytran@gmail.com','1',3);
-Insert Into Cart(Username, ProductID, Quantity) Values('duytran@gmail.com','2',3);
-Insert Into Cart(Username, ProductID, Quantity) Values('duytran@gmail.com','4',3);
-Insert Into Cart(Username, ProductID, Quantity) Values('duytran@gmail.com','5',3);
-Insert Into Cart(Username, ProductID, Quantity) Values('duytran1@gmail.com','1',3);
-Insert Into Cart(Username, ProductID, Quantity) Values('duytran1@gmail.com','2',2);
-Insert Into Cart(Username, ProductID, Quantity) Values('duytran1@gmail.com','3',4);
+Insert Into cart(Username, ProductID, Quantity) Values('duytran@gmail.com','1',3);
+Insert Into cart(Username, ProductID, Quantity) Values('duytran@gmail.com','2',3);
+Insert Into cart(Username, ProductID, Quantity) Values('duytran@gmail.com','4',3);
+Insert Into cart(Username, ProductID, Quantity) Values('duytran@gmail.com','5',3);
+Insert Into cart(Username, ProductID, Quantity) Values('duytran1@gmail.com','1',3);
+Insert Into cart(Username, ProductID, Quantity) Values('duytran1@gmail.com','2',2);
+Insert Into cart(Username, ProductID, Quantity) Values('duytran1@gmail.com','3',4);
 
 
 -- Data Rating
-Insert Into Rating(Username, ProductID, Rating) Values('duytran1@gmail.com','1',4.3);
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','1','5');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','3','5');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','4','3.5');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','5','3.5');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','7','3.9');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','8','3.8');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','9','3.5');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','10','4.6');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','13','4.4');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','15','4.2');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','17','5');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','18','3.8');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','19','4.7');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','20','4.6');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','21','4.5');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran@gmail.com','22','4');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran1@gmail.com','21','4.3');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran1@gmail.com','22','3');
-Insert Into Rating(Username,ProductID,Rating) Values('duytran1@gmail.com','20','4');
+Insert Into rating(Username, ProductID, Rating) Values('duytran1@gmail.com','1',4.3);
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','1','5');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','3','5');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','4','3.5');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','5','3.5');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','7','3.9');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','8','3.8');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','9','3.5');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','10','4.6');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','13','4.4');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','15','4.2');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','17','5');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','18','3.8');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','19','4.7');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','20','4.6');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','21','4.5');
+Insert Into rating(Username,ProductID,Rating) Values('duytran@gmail.com','22','4');
+Insert Into rating(Username,ProductID,Rating) Values('duytran1@gmail.com','21','4.3');
+Insert Into rating(Username,ProductID,Rating) Values('duytran1@gmail.com','22','3');
+Insert Into rating(Username,ProductID,Rating) Values('duytran1@gmail.com','20','4');
 
-
-   
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
